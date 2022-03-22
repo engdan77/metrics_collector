578578f42b3c1fc95b0b87d3c938d06ab4f05f46
Daniel Engvall <daniel@engvalls.eu>
2022-03-22 22:53:24 +0100

    Refactoring AppleHealth

--- my_health_stats/applehealth.py
+++ my_health_stats/applehealth.py
@@ -1,13 +1,14 @@
 import json
 from collections import defaultdict
 from tempfile import NamedTemporaryFile
+from typing import Any
 from zipfile import ZipFile
-
 from apple_health import HealthData
 from loguru import logger
+from my_health_stats.base import DaysActivities, BaseService
 
 
-class AppleHealth:
+class AppleHealth(BaseService):
     def __init__(self, zip_file: str):
         self.zip_file = zip_file
         self.activity_prefix = "HKQuantityTypeIdentifier"
@@ -21,49 +22,49 @@ class AppleHealth:
             "HKQuantityTypeIdentifierStepCount",
         ]
 
-        self.xml = self.extract_xml(self.zip_file)
+    def parse_records(self):
+        self.xml = self._extract_xml(self.zip_file)
         logger.debug(f"found xml {int(len(self.xml) / 1000)} KB")
-        self.records = self.get_health_data_from_xml(self.xml)
-        self.result = self.get_daily_records(self.records)
+        self.records = self._get_health_data_from_xml(self.xml)
 
-    def extract_xml(self, input_zip):
+    def _extract_xml(self, input_zip):
         with ZipFile(input_zip) as myzip:
             logger.debug(myzip.namelist())
             with myzip.open("apple_health_export/export.xml") as myfile:
                 return myfile.read()
 
-    def get_health_data_from_xml(self, input_xml: bytes):
+    def _get_health_data_from_xml(self, input_xml: bytes):
         with NamedTemporaryFile() as fp:
             fp.write(input_xml)
             health_data = HealthData.read(fp.name)
         return health_data
 
-    def get_all_types(self, input_records: HealthData, year=2022):
+    def _get_all_types(self, input_records: HealthData, year=2022):
         name_types = set()
         for r in input_records.records:
             if r.start.year == year:
                 name_types.add(r.name)
         return name_types
 
-    def get_one_each_type(self, input_records):
+    def _get_one_each_type(self, input_records):
         result = {}
         for r in input_records.records:
             result[r.name] = r
         return result
 
-    def get_daily_records(self, input_records: HealthData):
+    def get_data_from_service(self, date_) -> DaysActivities:
+        self.parse_records()
+        input_records = self.records
         result = defaultdict(lambda: defaultdict(dict))
         for r in input_records.records:
             if r.name in self.activities:
-                dt = r.start.strftime("%Y-%m-%d")
-                activity_name = r.name.replace(self.activity_prefix, "")
-                result[dt][activity_name]["unit"] = r.unit
-                values = result[dt][activity_name].get("value", [])
+                if r.start == date_:
+                    continue
+                d = r.start.strftime("%Y-%m-%d")
+                activity_name = r.name.replace(self.activity_prefix, "").lower()
+                result[d][activity_name]["unit"] = r.unit
+                values = result[d][activity_name].get("value", [])
                 values.append(r.value)
-                result[dt][activity_name]["value"] = values
-        return result
+                result[d][activity_name]["value"] = values
+        return dict(result)
 
-    def to_json(self, filename: str):
-        with open(filename, "w") as f:
-            logger.debug(f'writing to {filename}')
-            json.dump(self.result, f, indent=2)
