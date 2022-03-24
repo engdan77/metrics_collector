5adaed7017c45436630be83db7d0edcb56f303d5
Daniel Engvall <daniel@engvalls.eu>
2022-03-24 21:03:49 +0100

    Exception handler for GarminConnect

--- my_health_stats/garmin.py
+++ my_health_stats/garmin.py
@@ -1,9 +1,10 @@
 from collections import defaultdict
 
-from garminconnect import Garmin
+from garminconnect import Garmin, GarminConnectConnectionError
 from loguru import logger
 from my_health_stats.base import DaysActivities, BaseService
 from datetime import date, datetime
+import time
 
 
 class MyGarmin(BaseService):
@@ -12,8 +13,16 @@ class MyGarmin(BaseService):
         self.api = Garmin(username, password)
         self.logged_in = False
 
-    def login(self):
-        self.logged_in = self.api.login()
+    def login(self, retries=5, timer=2):
+        for _ in range(1, retries + 1):
+            try:
+                self.logged_in = self.api.login()
+                logger.debug('Successfully logged in to GarminConnect')
+                break
+            except GarminConnectConnectionError:
+                sleep_time = _ * timer
+                logger.warning(f'Failed attempt {_}, sleep for {sleep_time} secs')
+                time.sleep(sleep_time)
 
     def get_data_from_service(self, date_: str) -> DaysActivities:
         if not self.logged_in:
