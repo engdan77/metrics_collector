831ab6b22eaa757a0fc602530e3208cadefae13b
Daniel Engvall <daniel@engvalls.eu>
2022-03-24 21:04:12 +0100

    Initial sample

--- my_health_stats/__main__.py
+++ my_health_stats/__main__.py
@@ -1,19 +1,24 @@
 from my_health_stats.applehealth import AppleHealth
 from my_health_stats.garmin import MyGarmin
+from my_health_stats.utils import get_past_days
 import os
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 def main():
     ah = AppleHealth('./data/health_data_2022.zip')
-    # ah.to_json('health.json')
-    i = ah.get_data('2021-08-07')
-    print(i)
-
     g = MyGarmin(os.getenv('USERNAME'), os.getenv('PASSWORD'))
-    j = g.get_data('2021-08-07')
-    print(j)
+    number_of_days = 90
+
+    for i, day in enumerate(get_past_days(number_of_days)):
+        for f in (ah, g):
+            logger.debug(f'Processing {d} / {number_of_days} ')
+            result = f.get_data(day)
+            print(result)
+
 
 if __name__ == '__main__':
     main()
