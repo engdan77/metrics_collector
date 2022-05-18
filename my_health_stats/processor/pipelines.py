4fd92475e9ed3a02eca86ff56e8d2dc791d09858
Daniel Engval <daniel@engvalls.eu>
2022-05-18 21:20:43 +0200

    Adding pipelines gluing things together

---
+++ my_health_stats/processor/pipelines.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+
+from my_health_stats.extract.apple import AppleHealthExtract
+from my_health_stats.extract.base import BaseExtract
+from my_health_stats.extract.garmin import GarminExtract
+from my_health_stats.load.base import BaseLoadGraph
+from my_health_stats.load.graph import GarminAppleLoadGraph
+from my_health_stats.processor.base import BasePipeline
+from my_health_stats.transform.base import BaseTransform
+from my_health_stats.transform.transformers import GarminAppleTransform
+import pandas as pd
+from typing import Type
+
+
+class GarminApplePipeline(BasePipeline):
+
+    def __init__(self, apple_health_path='../data/export.zip', garmin_user=os.getenv('USERNAME'), garmin_password=os.getenv('PASSWORD')):
+        self.garmin_password = garmin_password
+        self.garmin_user = garmin_user
+        self.apple_health_path = apple_health_path
+
+    def extract_and_transform(self) -> GarminAppleTransform:
+        apple_bytes = Path(self.apple_health_path).read_bytes()
+        ah = AppleHealthExtract(apple_bytes)
+        g = GarminExtract(self.garmin_user, self.garmin_password)
+        return GarminAppleTransform(ah, g)
+
+    def get_graph_class(self) -> Type[BaseLoadGraph]:
+        return GarminAppleLoadGraph
---
+++ my_health_stats/transform/transformers.py
@@ -0,0 +1,88 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+import pandera as pa
+
+from my_health_stats.extract.apple import AppleHealthExtract
+from my_health_stats.extract.garmin import GarminExtract
+from my_health_stats.transform.base import BaseTransform, TransformError
+
+
+class GarminAppleTransform(BaseTransform):
+    input_schema = pa.DataFrameSchema(
+        {
+            "distancewalkingrunning_km_sum": pa.Column(float, nullable=True),
+            "bodymass_kg": pa.Column(float, nullable=True),
+            "bloodpressuresystolic_mmHg": pa.Column(float, nullable=True),
+            "bloodpressurediastolic_mmHg": pa.Column(float, nullable=True),
+            "distancewalkingrunning_km": pa.Column(float, nullable=True),
+            "running_distance_meters": pa.Column(float, nullable=True),
+            "running_duration_seconds": pa.Column(float, nullable=True),
+            "walking_distance_meters": pa.Column(float, nullable=True),
+        },
+    )
+
+    def __init__(self, apple_df: AppleHealthExtract, garmin_df: GarminExtract):
+        self.df = pd.concat([apple_df.to_df(), garmin_df.to_df()])
+
+    def process_pipeline(self, from_: datetime.date, to_: datetime.date) -> pd.DataFrame:
+        (
+            self.index_as_dt()
+            .aggregate_combined_dataframes()
+            .filter_period(from_, to_)
+            .add_col_avg_speed_running_trip()
+            .add_apple_garmin_distances()
+            .add_missing_values()
+        )
+        return self.df
+
+    def add_col_avg_speed_running_trip(
+        self, trip_distance_meters=6000, margin_percentage=8
+    ):
+        """Adding extra column for running speed based on e.g. 6Km runs"""
+        diff_meters = (margin_percentage / 100) * trip_distance_meters
+        try:
+            self.df["avg_speed_running_trip"] = self.df.apply(
+                lambda row: (row.running_duration_seconds / 60)
+                / (row.running_distance_meters / 1000)
+                if row.running_distance_meters
+                and row.running_distance_meters - diff_meters
+                <= row.running_distance_meters
+                <= row.running_distance_meters + diff_meters
+                else np.NaN,
+                axis=1,
+            )
+        except AttributeError:
+            raise TransformError
+        return self
+
+    def add_apple_garmin_distances(self):
+        """Adding columns combining distances from both Garmin and Apple"""
+        self.df["running_distance_garmin"] = self.df.running_distance_meters.apply(
+            lambda x: x / 1000 if not pd.isna(x) else 0
+        )
+        self.df["walking_distance_garmin"] = self.df.walking_distance_meters.apply(
+            lambda x: x / 1000 if not pd.isna(x) else 0
+        )
+        self.df[
+            "walking_running_distance_applehealth"
+        ] = self.df.distancewalkingrunning_km_sum.apply(
+            lambda x: x if not pd.isna(x) else 0
+        )
+
+        self.df["walking_km"] = self.df.apply(
+            lambda row: row.walking_running_distance_applehealth
+            - row.running_distance_garmin
+            if row.walking_running_distance_applehealth
+            >= row.walking_distance_garmin + row.running_distance_garmin
+            else row.walking_running_distance_applehealth,
+            axis=1,
+        )
+        self.df["running_km"] = self.df.apply(
+            lambda row: row.running_distance_garmin, axis=1
+        )
+        self.df["total_distance_km"] = self.df.apply(
+            lambda row: row.running_km + row.walking_km, axis=1
+        )
+        return self
