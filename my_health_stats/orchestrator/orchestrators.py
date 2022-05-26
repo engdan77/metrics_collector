eacdf5db9b67a42c335550c5e4c683838e9de30a
Daniel Engval <daniel@engvalls.eu>
2022-05-26 20:05:02 +0200

    Temp disabling

--- my_health_stats/orchestrator/orchestrators.py
+++ my_health_stats/orchestrator/orchestrators.py
@@ -10,18 +10,22 @@ from my_health_stats.transform.transformers import GarminAppleTransform
 from typing import Type
 
 
-class GarminAppleOrchestrator(Orchestrator):
-
-    def __init__(self, apple_health_path='../data/export.zip', garmin_user=os.getenv('USERNAME'), garmin_password=os.getenv('PASSWORD')):
-        self.garmin_password = garmin_password
-        self.garmin_user = garmin_user
-        self.apple_health_path = apple_health_path
-
-    def extract_and_transform(self) -> GarminAppleTransform:
-        apple_bytes = Path(self.apple_health_path).read_bytes()
-        ah = AppleHealthExtract(apple_bytes)
-        g = GarminExtract(self.garmin_user, self.garmin_password)
-        return GarminAppleTransform(ah, g)
-
-    def get_graph_class(self) -> Type[BaseLoadGraph]:
-        return GarminAppleLoadGraph
+# class GarminAppleOrchestrator(Orchestrator):
+#     def __init__(
+#         self,
+#         apple_health_path="../data/export.zip",
+#         garmin_user=os.getenv("USERNAME"),
+#         garmin_password=os.getenv("PASSWORD"),
+#     ):
+#         self.garmin_password = garmin_password
+#         self.garmin_user = garmin_user
+#         self.apple_health_path = apple_health_path
+#
+#     def extract_and_transform(self) -> GarminAppleTransform:
+#         apple_bytes = Path(self.apple_health_path).read_bytes()
+#         ah = AppleHealthExtract(apple_bytes)
+#         g = GarminExtract(self.garmin_user, self.garmin_password)
+#         return GarminAppleTransform(ah, g)
+#
+#     def get_graph_class(self) -> Type[BaseLoadGraph]:
+#         return GarminAppleLoadGraph
