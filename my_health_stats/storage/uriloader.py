612a8a8caf6794814e5882dab27c660c958c9a74
Daniel Engval <daniel@engvalls.eu>
2022-05-26 20:04:20 +0200

    Adding uri loader to abstract loading bytes to extractor.

---
+++ my_health_stats/storage/uriloader.py
@@ -0,0 +1,22 @@
+import os
+from dataclasses import dataclass
+from urllib.parse import urlparse, urljoin
+import fs
+from furl import furl
+
+
+def uri_loader(uri_string) -> bytes:
+    uri_parts = furl(uri_string)
+    p = uri_parts.pathstr
+    dirname = os.path.dirname(p)
+    filename = os.path.basename(p)
+    uri_parts.path = dirname
+    with fs.open_fs(uri_parts.tostr()) as p:
+        return p.open(filename, 'rb').read()
+
+
+if __name__ == '__main__':
+    access_token = os.getenv('DROPBOX_ACCESS_TOKEN')
+    file_path = os.getenv('FILEPATH')
+    u = UriLoader(f'dropbox://dropbox.com{file_path}?access_token={access_token}')
+    print(len(u.get_bytes()))
