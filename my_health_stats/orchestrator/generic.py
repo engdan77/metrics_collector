71f79e436b0c34adc4c095000cdf239e1b067f58
Daniel Engval <daniel@engvalls.eu>
2022-05-28 09:53:45 +0200

    Adding extra params method to orchestrator and enum for types

--- my_health_stats/orchestrator/generic.py
+++ my_health_stats/orchestrator/generic.py
@@ -1,10 +1,19 @@
+from __future__ import annotations  # required to avoid circular imports for typing purposes
 import datetime
-from abc import ABC, abstractmethod
 from collections import defaultdict
+from typing import Type, Annotated
+import my_health_stats
+from enum import Enum, auto
+from typing import TYPE_CHECKING
 
-# from my_health_stats.load.base import BaseLoadGraph
-# from my_health_stats.transform.base import BaseTransform
-from typing import Type, Annotated, Iterable
+if TYPE_CHECKING:
+    from my_health_stats.extract.base import parameter_dict, BaseExtract  # only when typing
+
+
+class ClassType(str, Enum):
+    extract = auto()
+    transform = auto()
+    load = auto()
 
 
 def register_dag_name(cls):
@@ -20,8 +29,23 @@ class Orchestrator:
         Annotated[str, "dag name"], list[Annotated[Type, "classes"]]
     ] = defaultdict(list)
 
-    def extract_and_transform(self):
-        """Code for extracting e.g. using parameters in __init__"""
+    def __init__(self):
+        self.type = {ClassType.extract: my_health_stats.extract.base.BaseExtract,
+                     ClassType.transform: my_health_stats.transform.base.BaseTransform,
+                     ClassType.load: my_health_stats.load.base.BaseLoadGraph}
+
+    def get_extract_parameters(self) -> dict[BaseExtract, parameter_dict]:
+        result = {}
+        for dag_name in self.registered_etl_entities.keys():
+            extract_classes = self.get_classes(dag_name, ClassType.extract)
+            for cls in extract_classes:
+                result[cls] = cls.get_parameters()
+        return result
+
+    def get_classes(self, dag_name, class_type: ClassType) -> list[BaseExtract]:
+        return [cls for cls in self.registered_etl_entities.get(dag_name, []) if cls.__base__ is self.type.get(class_type, None)]
+
+    def run_dag(self, dag_name):
         ...
 
     def get_graph_class(self):
