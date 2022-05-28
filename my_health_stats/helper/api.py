7e50838b337db19447a1b3dc4201cff63b0d6007
Daniel Engval <daniel@engvalls.eu>
2022-05-28 09:52:34 +0200

    Preparing generic async exception handler

--- my_health_stats/helper/api.py
+++ my_health_stats/helper/api.py
@@ -1,4 +1,6 @@
 import importlib
+from loguru import logger
+import sys
 
 
 def import_item(item):
@@ -11,3 +13,16 @@ def import_item(item):
     else:
         f = item
     return f
+
+
+def asyncio_exception_handler(loop, context):
+    print('asyncio_exception_handler called.')
+    exception = context.get('exception', None)
+    if exception:
+        exc_info = (type(exception), exception, exception.__traceback__)
+        if issubclass(exception.__class__, KeyboardInterrupt):
+            sys.__excepthook__(*exc_info)
+            return
+        logger.error(f'Uncaught Exception: {exc_info[0].__name__}')
+    else:
+        logger.error(context['message'])
