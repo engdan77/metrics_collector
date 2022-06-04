143408f547ecd408573616ea466c9740056078e1
Daniel Engval <daniel@engvalls.eu>
2022-06-04 12:23:21 +0200

    Adding got to test extraction

--- my_health_stats/web/ui.py
+++ my_health_stats/web/ui.py
@@ -1,16 +1,18 @@
 import datetime
 from typing import Iterable, Type, Annotated
 
+import pywebio.input
 from pywebio.input import input, FLOAT, radio
 from pywebio.output import put_text
 
-from my_health_stats.extract.base import BaseExtractParameters
+from my_health_stats.extract.base import BaseExtractParameters, BaseExtract
 from my_health_stats.load.base import BaseLoadGraph
 from my_health_stats.orchestrator.generic import Orchestrator, ClassType
 from loguru import logger
 
 from my_health_stats.transform import BaseTransform
 
+Params = dict[Annotated[str, "param name"], Annotated[str, "value"]]
 
 def space_shifter(text: str) -> str:
     if '_' in text:
@@ -19,9 +21,20 @@ def space_shifter(text: str) -> str:
         return text.replace(' ', '_').lower()
 
 
-def ui_get_arguments(param_defintion: dict[Annotated[str, "parm name"], Type]) -> BaseExtractParameters:
-    ...
-    # TODO: prompt and return
+def ui_get_params(param_defintion: dict[Annotated[str, "parm name"], Type]) -> Params:
+    """Take dict and get data using Web UI"""
+    args = {}
+    for label, type_ in param_defintion.items():
+        match type_.__name__:
+            case 'str' | 'int':
+                args[label] = pywebio.input.input(label)
+    return args
+
+
+def dict_to_extract_params_object(params: Params, extract_class: BaseExtract) -> BaseExtractParameters:
+    cls = extract_class.get_extract_parameter_class()
+    obj = cls(**params)
+    return obj
 
 
 def main_ui():
@@ -33,14 +46,17 @@ def main_ui():
     extract_classes = o.get_registered_classes(dag_name, ClassType.extract)
 
     # create extract objects
+    extract_objects = []
     for extract_class in extract_classes:
-        get_args = args[dag_name][extract_class]
-        logger.debug(f"get arguments for {extract_class=} which is {get_args}")
-        ...
-        extract_object = extract_class(...)  # add args
+        extract_args = args[dag_name][extract_class]
+        logger.debug(f"get arguments for {extract_class=} which is {extract_args}")
+        a = ui_get_params(extract_args)
+        p = dict_to_extract_params_object(a, extract_class)
+        extract_object = extract_class(p)  # add args
+        extract_objects.append(extract_object)
 
-    transfomer_class: BaseTransform = o.get_registered_classes(dag_name, ClassType.transform, only_first=True)
-    t = transfomer_class(*extract_objects)
+    transformer_class: BaseTransform = o.get_registered_classes(dag_name, ClassType.transform, only_first=True)
+    t = transformer_class(*extract_objects)
     load_class: BaseLoadGraph = o.get_registered_classes(dag_name, ClassType.load, only_first=True)
     from_ = datetime.date(2022, 1, 1)
     to_ = datetime.date(2022, 1, 3)
