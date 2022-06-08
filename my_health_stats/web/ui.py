2a27925fc1cdecd5e138397a0da9e983357f08b8
Daniel Engval <daniel@engvalls.eu>
2022-06-08 21:25:12 +0200

    Finalizing UI

--- my_health_stats/web/ui.py
+++ my_health_stats/web/ui.py
@@ -3,7 +3,7 @@ from typing import Iterable, Type, Annotated
 
 import pywebio.input
 from pywebio.input import input, FLOAT, radio
-from pywebio.output import put_text
+from pywebio.output import put_html
 
 from my_health_stats.extract.base import BaseExtractParameters, BaseExtract
 from my_health_stats.load.base import BaseLoadGraph
@@ -39,13 +39,15 @@ def dict_to_extract_params_object(params: Params, extract_class: BaseExtract) ->
 
 def main_ui():
     o = Orchestrator()
+
+    # get which dag
     args = o.get_extract_parameters()
     logger.debug(args)
     dag_name = space_shifter(radio("Chose what to extract", options=[space_shifter(str(arg)) for arg in args.keys()]))
     logger.debug(f'{dag_name=}')
-    extract_classes = o.get_registered_classes(dag_name, ClassType.extract)
 
     # create extract objects
+    extract_classes = o.get_registered_classes(dag_name, ClassType.extract)
     extract_objects = []
     for extract_class in extract_classes:
         extract_args = args[dag_name][extract_class]
@@ -55,14 +57,19 @@ def main_ui():
         extract_object = extract_class(p)  # add args
         extract_objects.append(extract_object)
 
-    transformer_class: BaseTransform = o.get_registered_classes(dag_name, ClassType.transform, only_first=True)
-    t = transformer_class(*extract_objects)
-    load_class: BaseLoadGraph = o.get_registered_classes(dag_name, ClassType.load, only_first=True)
-    from_ = datetime.date(2022, 1, 1)
-    to_ = datetime.date(2022, 1, 3)
-    l = load_class(t, from_, to_)
-    graphs = o.get_graph_methods(dag_name)
-    logger.debug(graphs)
+    # create transform object
+    transformer_class = o.get_registered_classes(dag_name, ClassType.transform, only_first=True)
+    transformer_instance = transformer_class(*extract_objects)
+
+    # create load/grpah object
+    load_class: Type[BaseLoadGraph] = o.get_registered_classes(dag_name, ClassType.load, only_first=True)
+    from_ = input('From date', type='date')
+    to_ = input('To date', type='date')
+    load_instance = load_class(transformer_instance, from_, to_)
+    graph_methods = load_instance.get_all_graph_methods()
+    logger.debug(graph_methods)
+    for graph in load_instance.get_all_graph_methods():
+        put_html(load_instance.to_html(graph))
 
     # height = input("Input your height(cm)：", type=FLOAT)
     # weight = input("Input your weight(kg)：", type=FLOAT)
