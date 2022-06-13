3ce07c611f6731205a5d481915d17d15724a331a
Daniel Engval <daniel@engvalls.eu>
2022-06-13 22:45:04 +0200

    Working on the magic of dynamically creating endpoints

--- my_health_stats/web/rest.py
+++ my_health_stats/web/rest.py
@@ -3,6 +3,8 @@ from loguru import logger
 from makefun import create_function
 from fastapi import APIRouter
 from my_health_stats.orchestrator.generic import Orchestrator
+import datetime
+from typing import Union
 
 graph_router = APIRouter()
 o = Orchestrator()
@@ -15,12 +17,17 @@ def graph(**args):
 
 
 # Some magic to dynamically create API endpoints
-for _ in o.get_dag_names():
-    dag_args = o.get_extract_parameters().get(_)
+for dag_name in o.get_dag_names():
+    dag_args = o.get_extract_parameters().get(dag_name)
     all_args = []
-    for cls, args in dag_args.items():
+    for cls, args in dag_args.items():  # add args for extract class
         for arg, type_ in args.items():
             all_args.append(f'{arg}: {type_.__name__}')
-    gen_func = create_function(f'{_}({", ".join(all_args)})', graph)
-    graph_router.add_api_route(f'/{_}', gen_func)
+    graph_names = o.get_graph_names(dag_name)  # get all graph names options
+    GraphEnum = StrEnum('GraphEnum', graph_names)
+    all_args.append(f'graph: GraphEnum')
+    for _ in ('from_date', 'to_date'):
+        all_args.append(f'{_}: datetime.date')
+    gen_func = create_function(f'{dag_name}({", ".join(all_args)})', graph)
+    graph_router.add_api_route(f'/{dag_name}', gen_func)
 
