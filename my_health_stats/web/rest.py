bc0578194c14f8d70e170aba06504f89c26f7017
Daniel Engval <daniel@engvalls.eu>
2022-06-16 20:29:22 +0200

    Adjusting date arguments

--- my_health_stats/web/rest.py
+++ my_health_stats/web/rest.py
@@ -4,7 +4,7 @@ from makefun import create_function
 from fastapi import APIRouter
 from my_health_stats.orchestrator.generic import Orchestrator
 import datetime
-from typing import Union
+from typing import Union, Optional
 
 graph_router = APIRouter()
 o = Orchestrator()
@@ -20,14 +20,18 @@ def graph(**args):
 for dag_name in o.get_dag_names():
     dag_args = o.get_extract_parameters().get(dag_name)
     all_args = []
-    for cls, args in dag_args.items():  # add args for extract class
-        for arg, type_ in args.items():
-            all_args.append(f'{arg}: {type_.__name__}')
+
     graph_names = o.get_graph_names(dag_name)  # get all graph names options
     GraphEnum = StrEnum('GraphEnum', graph_names)
     all_args.append(f'graph: GraphEnum')
-    for _ in ('from_date', 'to_date'):
-        all_args.append(f'{_}: datetime.date')
+
+    for arg_name, default_date in (('from_date', 'today() - datetime.timedelta(days=1)'), ('to_date', 'today()')):
+        all_args.append(f'{arg_name}: datetime.date = datetime.date.{default_date}')
+
+    for cls, args in dag_args.items():  # add args for extract class
+        for arg, type_ in args.items():
+            all_args.append(f'{arg}: {type_.__name__} | None=None')
+
     gen_func = create_function(f'{dag_name}({", ".join(all_args)})', graph)
     graph_router.add_api_route(f'/{dag_name}', gen_func)
 
