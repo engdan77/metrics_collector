a9f5b46da43ea55c4efd134d962e800c01541989
Daniel Engval <daniel@engvalls.eu>
2022-07-10 20:36:57 +0200

    Refactoring

--- metrics_collector/orchestrator/generic.py
+++ metrics_collector/orchestrator/generic.py
@@ -30,6 +30,7 @@ def register_dag_name(cls):
 
 
 class Orchestrator:
+    """Orchestrator acting as proxy for the ETL processes"""
 
     registered_etl_entities: defaultdict[
         Annotated[str, "dag name"], list[Annotated[Type, "classes"]]
@@ -41,37 +42,32 @@ class Orchestrator:
                      ClassType.load: metrics_collector.load.base.BaseLoadGraph}
 
     def get_dag_names(self) -> list:
+        """Get what registered services"""
         return list(self.registered_etl_entities.keys())
 
-    def get_extract_parameters(self) -> dict[Annotated[str, 'dag_name'], dict[str, parameter_dict]]:
+    def get_extract_services_and_parameters(self) -> dict[Annotated[str, 'dag_name'], dict[str, parameter_dict]]:
+        """Get what registered services and their parameters"""
         result = defaultdict(dict)
         for dag_name in self.registered_etl_entities.keys():
-            extract_classes = self.get_registered_classes(dag_name, ClassType.extract)
+            extract_classes = self._get_registered_classes(dag_name, ClassType.extract)
             for cls in extract_classes:
                 result[dag_name][cls] = cls.get_parameters()
         return result
 
     def get_graph_names(self, dag_name: str) -> list[str]:
-        extract_classes = self.get_registered_classes(dag_name, ClassType.load)
+        extract_classes = self._get_registered_classes(dag_name, ClassType.load)
         methods = []
         for cls in extract_classes:
             for m in cls.get_all_graph_methods(cls):
                 methods.append(m.__name__)
         return methods
 
-    def get_graph_methods(self, dag_name: str) -> Iterable[Annotated[Callable, "Class methods generating graphs"]]:
-        extract_classes = self.get_registered_classes(dag_name, ClassType.load)
-        methods = []
-        for cls in extract_classes:
-            methods.append(cls.get_all_graph_methods())
-        return methods
-
-    def get_registered_classes(self, dag_name, class_type: ClassType, only_first=False) -> list[Type[BaseExtract] | Type[BaseTransform] | Type[BaseLoadGraph]] | Type[BaseExtract] | Type[BaseTransform] | Type[BaseLoadGraph]:
+    def _get_registered_classes(self, dag_name, class_type: ClassType, only_first=False) -> list[Type[BaseExtract] | Type[BaseTransform] | Type[BaseLoadGraph]] | Type[BaseExtract] | Type[BaseTransform] | Type[BaseLoadGraph]:
         classes = [cls for cls in self.registered_etl_entities.get(dag_name, []) if cls.__base__ is self.type.get(class_type, None)]
         return next(iter(classes)) if only_first else classes
 
     @staticmethod
-    def dict_to_extract_params_object(params: Params, extract_class: BaseExtract) -> BaseExtractParameters:
+    def _dict_to_extract_params_object(params: Params, extract_class: BaseExtract) -> BaseExtractParameters:
         cls = extract_class.get_extract_parameter_class()
         declare_params = [k for k, v in cls.__dataclass_fields__.items() if v.init]
         # only pass params that are valid for the class
@@ -80,21 +76,21 @@ class Orchestrator:
 
     def get_extract_args_def(self, dag_name):
         """Used to e.g. pass to UI to request params from use"""
-        args = self.get_extract_parameters()
-        extract_classes = self.get_registered_classes(dag_name, ClassType.extract)
+        args = self.get_extract_services_and_parameters()
+        extract_classes = self._get_registered_classes(dag_name, ClassType.extract)
         for extract_class in extract_classes:
             yield args[dag_name][extract_class]
 
     def get_extract_objects(self, dag_name, extract_params: dict):
         """Main entrypoint for getting extract objects used to get transformer object"""
         # create extract objects
-        args = self.get_extract_parameters()
-        extract_classes = self.get_registered_classes(dag_name, ClassType.extract)
+        args = self.get_extract_services_and_parameters()
+        extract_classes = self._get_registered_classes(dag_name, ClassType.extract)
         extract_objects = []
         for extract_class in extract_classes:
             extract_args = args[dag_name][extract_class]
             logger.debug(f"get arguments for {extract_class=} which is {extract_args}")
-            p = self.dict_to_extract_params_object(extract_params, extract_class)
+            p = self._dict_to_extract_params_object(extract_params, extract_class)
             extract_object = extract_class(p)  # add args
             extract_objects.append(extract_object)
         return extract_objects
@@ -102,28 +98,14 @@ class Orchestrator:
     def get_transform_object(self, dag_name: str, extract_objects: list) -> BaseTransform:
         """Main entrypoint for getting transform object used to load graph from"""
         # create transform object
-        transformer_class = self.get_registered_classes(dag_name, ClassType.transform, only_first=True)
+        transformer_class = self._get_registered_classes(dag_name, ClassType.transform, only_first=True)
         transformer_object = transformer_class(*extract_objects)
         return transformer_object
 
     def get_all_graphs(self, from_: datetime.date | str, to_: datetime.date | str, dag_name: str, transform_object: BaseTransform, format: Annotated[str, "Type such as `html` or `png`"] = 'html') -> Generator[Any, None, None]:
         """Main entrypoint for getting all graph objects with methods such as .to_htm() or .to_png()"""
-        load_class: Type[BaseLoadGraph] = self.get_registered_classes(dag_name, ClassType.load, only_first=True)
+        load_class: Type[BaseLoadGraph] = self._get_registered_classes(dag_name, ClassType.load, only_first=True)
         load_instance = load_class(transform_object, from_, to_)
         for graph in load_instance.get_all_graph_methods():
             yield getattr(load_instance, f'to_{format}')(graph)
 
-
-    def run_dag(self, dag_name):
-        ...
-
-    def get_graph_class(self):
-        """Return a simple class"""
-        ...
-
-    def execute(self, start_date=datetime.date, end_date=datetime.date):
-        """This returns an object to be used to generate graphs in various formats"""
-        # transform_object: BaseTransform = self.extract_and_transform()
-        # graph_class = self.get_graph_class()
-        # graph_loader = graph_class(transform_object, start_date, end_date)
-        return graph_loader
