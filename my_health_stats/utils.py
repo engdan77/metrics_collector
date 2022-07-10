68680dabcd6d49c7b6cc5973d67bc5d9e039ea76
Daniel Engval <daniel@engvalls.eu>
2022-07-10 14:30:38 +0200

    Adding get_days_between

--- my_health_stats/utils.py
+++ my_health_stats/utils.py
@@ -1,5 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from typing import Generator
+import datetime
 
 
 def get_past_days(number_days: int = 1, offset=0) -> Generator:
@@ -7,3 +8,16 @@ def get_past_days(number_days: int = 1, offset=0) -> Generator:
     for d in range(1, number_days):
         x = today - timedelta(days=d + offset)
         yield x.strftime('%Y-%m-%d')
+
+
+def get_days_between(from_: str | datetime.date, to_: str | datetime.date, as_text=True, fmt='%Y-%m-%d'):
+    try:
+        from_ = datetime.datetime.strptime(from_, fmt)
+        to_ = datetime.datetime.strptime(to_, fmt)
+    except TypeError:
+        pass
+    days = (to_ - from_).days
+    for day in range(days):
+        r: datetime.date = from_ + timedelta(days=day)
+        yield r.strftime(fmt) if as_text else r
+
