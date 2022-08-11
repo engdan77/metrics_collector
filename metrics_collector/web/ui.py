d13525128b5d0874e1404d8dc0bf165016beb9d7
Daniel Engval <daniel@engvalls.eu>
2022-08-11 21:45:55 +0200

    Adding ability to use free text as date in UI - preparation for adding scheduler

--- metrics_collector/web/ui.py
+++ metrics_collector/web/ui.py
@@ -1,10 +1,14 @@
+import datetime
 import itertools
 from typing import Iterable, Type, Annotated, Protocol
 import pywebio.input
-from pywebio.input import input, FLOAT, radio, select
+from parsedatetime import Calendar
+from pywebio.input import input, radio, select, input_group
 from pywebio.output import put_html, put_processbar, set_processbar, put_text, clear
 from metrics_collector.orchestrator.generic import Orchestrator, ClassType, ProgressBar
 from loguru import logger
+
+from metrics_collector.utils import normalize_date
 
 Params = dict[Annotated[str, "param name"], Annotated[str, "value"]]
 
@@ -42,9 +46,30 @@ def ui_get_service_and_interval(o):
     logger.debug(args)
     dag_name = space_shifter(radio("Chose what to extract", options=[space_shifter(str(arg)) for arg in args.keys()]))
     logger.debug(f'{dag_name=}')
-    from_ = input('From date', type='date')
-    to_ = input('To date', type='date')
-    return dag_name, from_, to_
+
+    period = {'from': None, 'to': None}
+
+    def check_form(form_data):
+        now = datetime.date.today()
+        if form_data['date'] and form_data['text']:
+            return ('date', 'Chose one of the options')
+        if form_data['date'] and normalize_date(form_data['date']) <= now and not form_data['text']:
+            return None
+        if form_data['text']:
+            c = Calendar()
+            if c.parse(form_data['text'])[1] and normalize_date(form_data['text']) <= now:
+                return None
+            else:
+                return ('text', 'Not a valid date by text')
+        return ('date', 'Chose a date')
+
+    for t in ('from', 'to'):
+        data = input_group(f"{t.capitalize()} date:", [
+            input(f'Calendar...', type='date', name='date'),
+            input(f'...or text expression', name='text', type='text')
+        ], validate=check_form)
+        period[t] = data['date'] or data['text']
+    return dag_name, period['from'], period['to']
 
 
 def ui_get_extract_params(dag_name, orchestrator):
