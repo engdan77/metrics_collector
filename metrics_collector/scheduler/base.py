a52b62262a25b0bbb9cf3f8e3ebb8d9cd56130f4
Daniel Engval <daniel@engvalls.eu>
2022-09-14 20:06:02 +0200

    Adding shortener for BaseAction

--- metrics_collector/scheduler/base.py
+++ metrics_collector/scheduler/base.py
@@ -1,3 +1,4 @@
+import dataclasses
 from typing import Protocol
 from abc import ABC
 
@@ -7,6 +8,10 @@ class AsyncService(Protocol):
         ...
 
 
+@dataclasses.dataclass
 class BaseAction(ABC):
     def __repr__(self):
         return self.__dict__
+
+    def shorten(self, input_data, letters=8):
+        return f'{input_data[:letters]}...' if len(input_data) >= letters else input_data
