1c8e42445eaafac3425d121eb301d1a21ff4834a
Daniel Engval <daniel@engvalls.eu>
2022-09-14 18:49:03 +0200

    Adding BaseAction

--- metrics_collector/scheduler/base.py
+++ metrics_collector/scheduler/base.py
@@ -1,6 +1,12 @@
 from typing import Protocol
+from abc import ABC
 
 
 class AsyncService(Protocol):
     def start(self):
         ...
+
+
+class BaseAction(ABC):
+    def __repr__(self):
+        return self.__dict__
