5037528d81db88d03a376cdfb522965a4e98c828
Daniel Engval <daniel@engvalls.eu>
2022-10-19 20:08:23 +0200

    Cleaning up __main__

--- metrics_collector/__main__.py
+++ metrics_collector/__main__.py
@@ -1,14 +1,8 @@
-import asyncio
 import os
 import sys
-from pathlib import Path
-import datetime
 import typer
-from metrics_collector.extract.apple import AppleHealthExtract, AppleHealthExtractParameters
 from metrics_collector.extract.garmin import GarminExtract, GarminExtractParameters
 from metrics_collector.utils import get_past_days
-from metrics_collector.transform.transformers import GarminAppleTransform
-from metrics_collector.load.graph import GarminAppleLoadGraph
 from metrics_collector.scheduler import MyScheduler
 from metrics_collector.scheduler.tasks import default_initial_scheduled_tasks
 from metrics_collector.web.service import WebServer
@@ -30,31 +24,6 @@ def get_data(number_of_days=1800):
             logger.debug(f'Processing {i}/{number_of_days} ')
             result = f.get_data(day)
             print(result)
-
-
-def test_graphs():
-    # get_data(20)
-    apple_bytes = Path('../data/export.zip').read_bytes()
-    ah_params = AppleHealthExtractParameters(apple_bytes)
-    ah = AppleHealthExtract(ah_params)
-    g_params = GarminExtractParameters(os.getenv('USERNAME'), os.getenv('PASSWORD'))
-    g = GarminExtract(g_params)
-    _ = ah.get_data('2021-01-01')
-    _ = g.get_data('2021-01-01')
-    # df_ah = ah.to_df()
-    # df_g = g.to_df()
-    # df = pd.concat([df_ah, df_g])
-
-    x = GarminAppleTransform(ah, g)
-
-    # x.process_pipeline(datetime.date(2022, 1, 1), datetime.date(2022, 3, 1))
-    print(x)
-
-    graph_loader = GarminAppleLoadGraph(x, datetime.date(2021, 1, 1), datetime.date(2021, 3, 1))
-
-    # for graph_bytes in graph_loader.get_all_graphs(GraphFormat.html):
-    #     Path('/tmp/a.html').write_text(graph_bytes)
-    #     print('next')
 
 
 def start(port: int = 5050):
