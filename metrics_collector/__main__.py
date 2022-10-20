cb7bb25175dab545a2085417082711b023981ff3
Daniel Engval <daniel@engvalls.eu>
2022-10-20 09:35:10 +0200

    Adding parameters

--- metrics_collector/__main__.py
+++ metrics_collector/__main__.py
@@ -1,4 +1,6 @@
 import sys
+from enum import Enum
+
 import typer
 from metrics_collector.scheduler import MyScheduler
 from metrics_collector.scheduler.tasks import default_initial_scheduled_tasks
@@ -7,14 +9,19 @@ from loguru import logger
 import logging
 
 
+class LogLevel(str, Enum):
+    INFO = 'INFO'
+    DEBUG = 'DEBUG'
+
+
 def start_initial_loop(port):
     scheduler = MyScheduler(initials=default_initial_scheduled_tasks)
     WebServer((scheduler,), port=port)  # send services to be started with uvicorn
 
 
-def start(port: int = 5050):
+def start(port: int = 5050, log_level: LogLevel = LogLevel.INFO):
     logger.remove()
-    logger.add(sys.stdout, level=logging.DEBUG)
+    logger.add(sys.stdout, level=getattr(logging, log_level))
     start_initial_loop(port)
 
 
